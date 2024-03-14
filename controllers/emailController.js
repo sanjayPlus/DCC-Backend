7ad10b7b472947fe5458100346c05834d1361b46
sanjayPlus <sanjaysanthoshplusitpark@gmail.com>
2024-03-14 10:08:15 +0530

    dmc registeration added

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -8,86 +8,26 @@ const oAuth2Client = new google.auth.OAuth2(
 );
 oAuth2Client.setCredentials({ refresh_token: process.env.REFRESH_TOKEN });
 
-// async function sendMail(to, subject, text, html) {
-//   try {
-//     const transport = nodemailer.createTransport({
-//       host: process.env.MAIL_HOST, // Replace with your SMTP server host
-//       port: process.env.MAIL_PORT, // Replace with your SMTP port
-//       secure: false, // Set this to false when using STARTTLS with TLS
-//       auth: {
-//         user: process.env.SMAIL, // Your email address
-//         pass: process.env.MAIL_PASSWORD, // Your email password or app-specific password
-//       },
-//       requireTLS: true, // Enforces using TLS
-//       tls: {
-//         // Allowing self-signed certificates if necessary (use with caution)
-//         rejectUnauthorized: false, // Set to true to enforce certificate validation
-//       },
-//     });
-
-//     const mailOptions = {
-//       from:`${process.env.SITE_NAME} <${process.env.SMAIL}>`, // Sender address
-//       to, // Recipient
-//       subject,
-//       text,
-//       html: `
-//       <html>
-//       <head>
-//         <style>
-//           .container {
-//             width: 100%;
-//             height: max-content;
-//             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
-//           }
-//           .card {
-//             display: grid;
-
-//           }
-//           .card button{
-//             padding: 10px 25px;
-//             border: none;
-//             border-radius: 15px;
-//             background-color: rgb(12, 47, 145);
-//             color: aliceblue;
-//             box-shadow: 2px 2px 15px rgba(107, 95, 95, 0.334);
-//           }
-//         </style>
-//       </head>
-//       <body>
-//         <div class="container">
-//        ${html}
-//         </div>
-//       </body>
-//     </html>
-
-//       `,
-//     };
-
-//     const result = await transport.sendMail(mailOptions);
-//     return result;
-//   } catch (error) {
-//     return error;
-//   }
-// }
 async function sendMail(to, subject, text, html) {
   try {
-    const accessToken = await oAuth2Client.getAccessToken();
-
     const transport = nodemailer.createTransport({
-      service: "gmail",
+      host: process.env.MAIL_HOST, // Replace with your SMTP server host
+      port: process.env.MAIL_PORT, // Replace with your SMTP port
+      secure: false, // Set this to false when using STARTTLS with TLS
       auth: {
-        type: "OAuth2",
-        user: process.env.EMAIL,
-        clientId: process.env.CLIENT_ID,
-        clientSecret: process.env.CLIENT_SECRET,
-        refreshToken: process.env.REFRESH_TOKEN,
-        accessToken: accessToken,
+        user: process.env.SMAIL, // Your email address
+        pass: process.env.MAIL_PASSWORD, // Your email password or app-specific password
+      },
+      requireTLS: true, // Enforces using TLS
+      tls: {
+        // Allowing self-signed certificates if necessary (use with caution)
+        rejectUnauthorized: false, // Set to true to enforce certificate validation
       },
     });
 
     const mailOptions = {
-      from: `${process.env.SITE_NAME} <${process.env.EMAIL}>`,
-      to,
+      from:`${process.env.SITE_NAME} <${process.env.SMAIL}>`, // Sender address
+      to, // Recipient
       subject,
       text,
       html: `
@@ -101,7 +41,7 @@ async function sendMail(to, subject, text, html) {
           }
           .card {
             display: grid;
-           
+
           }
           .card button{
             padding: 10px 25px;
@@ -119,7 +59,7 @@ async function sendMail(to, subject, text, html) {
         </div>
       </body>
     </html>
-    
+
       `,
     };
 
@@ -129,6 +69,66 @@ async function sendMail(to, subject, text, html) {
     return error;
   }
 }
+// async function sendMail(to, subject, text, html) {
+//   try {
+//     const accessToken = await oAuth2Client.getAccessToken();
+
+//     const transport = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         type: "OAuth2",
+//         user: process.env.EMAIL,
+//         clientId: process.env.CLIENT_ID,
+//         clientSecret: process.env.CLIENT_SECRET,
+//         refreshToken: process.env.REFRESH_TOKEN,
+//         accessToken: accessToken,
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: `${process.env.SITE_NAME} <${process.env.EMAIL}>`,
+//       to,
+//       subject,
+//       text,
+//       html: `
+//       <html>
+//       <head>
+//         <style>
+//           .container {
+//             width: 100%;
+//             height: max-content;
+//             font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+//           }
+//           .card {
+//             display: grid;
+           
+//           }
+//           .card button{
+//             padding: 10px 25px;
+//             border: none;
+//             border-radius: 15px;
+//             background-color: rgb(12, 47, 145);
+//             color: aliceblue;
+//             box-shadow: 2px 2px 15px rgba(107, 95, 95, 0.334);
+//           }
+//         </style>
+//       </head>
+//       <body>
+//         <div class="container">
+//        ${html}
+//         </div>
+//       </body>
+//     </html>
+    
+//       `,
+//     };
+
+//     const result = await transport.sendMail(mailOptions);
+//     return result;
+//   } catch (error) {
+//     return error;
+//   }
+// }
 // sendMail('sanjaysanthosh919@gmail.com', 'Hello from gmail', 'Hello from gmail', '<h1>Hello from gmail</h1>')
 //   .then((result) => console.log('Email sent...', result))
 //   .catch((error) => console.log(error.message));
